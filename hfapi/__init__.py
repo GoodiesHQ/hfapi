871d14a4b21e077b2150064f1d243f9faac29447
GoodiesHQ <GoodiesHQ@users.noreply.github.com>
2020-07-20 15:50:26 -0700

    Update __init__.py

--- hfapi/__init__.py
+++ hfapi/__init__.py
@@ -5,19 +5,18 @@ HackForums API by Goodies
 from .hf_base import HFAppBase, HFClientBase
 # from .hf_sync import HFApp, HFClient
 from .hf_async import HFAppAsync, HFClientAsync
+from .helpers import API_VERSION as __version__
+
 from . import (
     asks, helpers,
 )
 
-
-from .helpers import API_VERSION as __version__
 __author__ = "Goodies <goodies@protonmail.com>"
-
 
 __all__ = [
     "HFClientBase", "HFAppBase",
     "HFAppAsync", "HFClientAsync",
     "asks",
     "helpers",
-    "__version__",
+    "__version__", "__author__",
 ]
