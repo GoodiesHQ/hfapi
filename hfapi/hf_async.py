d82580ae47b21e3279505c58c1f6e500a8a1608e
GoodiesHQ <aarcher73k@gmail.com>
2020-07-21 20:23:56 -0700

    Working on async implementation

--- hfapi/hf_async.py
+++ hfapi/hf_async.py
@@ -2,29 +2,52 @@
 Asynchronous Client implementation
 """
 
+from typing import Union
 from hfapi import asks
+from hfapi.scope import HFScope
 from .hf_base import HFAppBase, HFClientBase
-from .helpers import Response, call_api_async, craft_headers
+from .helpers import Response, call_api_async, craft_headers, require_scope, has
 import aiohttp
 import json
 
 
 class HFClientAsync(HFClientBase):
+    @require_scope("POSTSWRITE")
+    async def make_post(self, thread: Union[str, asks.Thread], message: str):
+        return asks.Post.load(
+            await call_api_async(
+                endpoint=self.url_write,
+                headers=craft_headers(self._access_token),
+                data=asks.to_data(
+                    asks.Post(
+                        tid=thread.tid if isinstance(thread, asks.Thread) else thread,
+                        message=message,
+                    )
+                ),
+            )
+        )
+
+    @require_scope("BASIC")
     async def me(self):
-        response = await call_api_async(
-            endpoint=self.url_read,
-            headers=craft_headers(self._access_token),
-            data={
-                "asks": json.dumps({"me":
-                    asks.Me.all().dict()
-                })
-            })
-        pprint(response.data)
+        ask = asks.Me.all()
+        if not has(self._scope, HFScope.ADV_R):
+            ask.lastactive = None
+            ask.unreadpms = None
+            ask.invisible = None
+            ask.totalpms = None
+            ask.warningpoints = None
+        return asks.Me.load(
+            await call_api_async(
+                endpoint=self.url_read,
+                headers=craft_headers(self._access_token),
+                data=asks.to_data(ask),
+            )
+        )
 
 
 class HFAppAsync(HFAppBase):
     async def authorize(self, code: str) -> HFClientAsync:
-        resp = await call_api_async(
+        data = await call_api_async(
             endpoint=self.url_auth,
             headers=craft_headers(),
             data={
@@ -34,6 +57,4 @@ class HFAppAsync(HFAppBase):
                 "code": code,
             }
         )
-        if not resp.success:
-            raise Exception(resp.error)
-        return HFClientAsync(resp.data.get("acces_token"))
+        return HFClientAsync(data.get("access_token"), data.get("scope"))
